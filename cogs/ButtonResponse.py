5b852d48e8f9d348c67cb104f0453ab0f383be87
nkc20236039 <nkc23.kawai.yuto@gmail.com>
2025-01-04 01:41:25 +0900

    Update: スレッドの完了、削除をボタンから行えるように更新

--- cogs/ButtonResponse.py
+++ cogs/ButtonResponse.py
@@ -8,6 +8,8 @@ if __name__ == "__main__":
     os.system(f"{sys.executable} main.py")
     
 import discord
+import re
+import asyncio
 from discord.ext import commands
 from Database import Database
 
@@ -44,7 +46,12 @@ class SupportForum(discord.ui.Modal):
         await thread.add_user(interaction.user)
 
         # スレッドに最初の投稿を作成
-        await thread.send(self.detail)
+        await thread.send(self.detail);
+        
+        # 利用数を記録
+        count = self.config.get_value(interaction.guild_id, "general", "used_count", default_value=0)
+        count += 1
+        self.config.set_value(interaction.guild_id, "general", "used_count", value=count)
 
         # 作成したスレッドへ案内
         await self.select_message.followup.send(f"プライベートチャットを作成しました。: {thread.jump_url}\n運営の対応をお待ちください。",ephemeral=True)
@@ -54,8 +61,13 @@ class SupportForum(discord.ui.Modal):
         if not operate_ch:
             return
         
+        # 完了ボタン
+        completion_button = discord.ui.Button(label="完了", style=discord.ButtonStyle.success, custom_id="support_completion")
+        view = discord.ui.View()
+        view.add_item(completion_button)
+        
         notice_ch = interaction.guild.get_channel(operate_ch)
-        await notice_ch.send(f"## 新しいお問い合わせがあります\n{title}\n{thread.jump_url}")
+        await notice_ch.send(f"## 新しいお問い合わせがあります\n{thread.jump_url}\nid:`{thread.id}`", view=view)
 
 class PremiseSelectMenu(discord.ui.View):
     def __init__(self, button_data:dict, format:str):
@@ -114,7 +126,6 @@ class PremiseSelectMenu(discord.ui.View):
             )
             await thread.add_user(interaction.user)
 
-            
             # 作成したスレッドへ案内
             await interaction.followup.send(f"プライベートチャットを作成しました。\n{thread.jump_url}へ移動してください。",ephemeral=True)
 
@@ -144,6 +155,10 @@ class ButtonResponse(commands.Cog):
 
                 elif interaction.data["custom_id"] == "purchase_cancel":
                     await self.send_purchase_cancel(interaction)
+                elif interaction.data["custom_id"] == "support_completion":
+                    await self.close_thread(interaction)
+                elif interaction.data["custom_id"] == "support_delete":
+                    await self.delete_thread(interaction)
 
                 else:
                     # ボタンの情報を取得する
@@ -168,24 +183,80 @@ class ButtonResponse(commands.Cog):
             await self.send_purchase_message(interaction, button_data)
 
     async def send_forum_message(self, interaction: discord.Interaction, button_data: dict):
-        await interaction.followup.send("お問い合わせありがとうございます。\nこちらのメッセージは自動送信です。あなたにのみ表示されており、時間経過で削除されます。\n\n対応をスムーズに行うためにお問い合わせ内容(カテゴリ)の選択と、カテゴリ選択後に表示されるフォーラムのタイトル入力を適切に行ってください。\nフォーラムを送信すると担当の者が交代して対応いたします。\n対応までに時間がかかる場合がございますがご了承ください。", ephemeral=True, view=PremiseSelectMenu(button_data, "forum"))
+        await interaction.followup.send("お問い合わせありがとうございます。\nこちらのメッセージは自動送信です。あなたにのみ表示されており、時間経過で削除されます。\n\n対応をスムーズに行うためにお問い合わせ内容(カテゴリ)の選択と、カテゴリ選択後に表示されるフォーラムのタイトル入力を適切に行ってください。\nフォーラムを送信すると担当者が交代して対応いたします。\n対応までに時間がかかる場合がございますがご了承ください。", ephemeral=True, view=PremiseSelectMenu(button_data, "forum"))
 
     async def send_purchase_message(self, interaction: discord.Interaction, button_data: dict):
         await interaction.followup.send("ご利用ありがとうございます。\nこちらのメッセージは自動送信です。あなたにのみ表示されており、時間経過で削除されます。\n購入手続きは運営が順を追って対応いたします。\n\n購入する内容(カテゴリ)の選択をすると運営とのプライベートチャットが開かれます。\nカテゴリの選択後、決定ボタンを押すことで購入内容が確定されます。", ephemeral=True, view=PremiseSelectMenu(button_data, "purchase"))
 
     async def send_purchase_ok(self, interaction: discord.Interaction):
         await interaction.edit_original_response(content=interaction.message.content, view=None)
-        await interaction.followup.send("ありがとうございます。\n担当の者が対応いたします。しばらくお待ちください。")
+        await interaction.followup.send("ありがとうございます。\n担当者が対応いたします。しばらくお待ちください。")
+
+        # 利用数を記録
+        count = self.config.get_value(interaction.guild_id, "general", "used_count", default_value=0)
+        count += 1
+        self.config.set_value(interaction.guild_id, "general", "used_count", value=count)
+
         # 運営用に通知
         operate_ch = self.config.get_value(interaction.guild_id, "general", "operate_ch")
         if not operate_ch:
             return
         
+        # 完了ボタン
+        completion_button = discord.ui.Button(label="完了", style=discord.ButtonStyle.success, custom_id="support_completion")
+        view = discord.ui.View()
+        view.add_item(completion_button)
+
         notice_ch = interaction.guild.get_channel(operate_ch)
-        await notice_ch.send(f"## 新しい購入手続きがあります\n{interaction.channel.name}\n{interaction.channel.jump_url}")
+        await notice_ch.send(f"## 新しい購入手続きがあります\n{interaction.channel.jump_url}\nid:`{interaction.channel.id}`", view=view)
 
     async def send_purchase_cancel(self, interaction: discord.Interaction):
         await interaction.channel.delete()
+
+    
+    async def close_thread(self, interaction: discord.Interaction):
+        # 押されたボタンから対象のスレッドを取得
+        pattern = r"\`(.*?)\`"
+        matches = re.findall(pattern, interaction.message.content)
+        completion_thread = interaction.guild.get_thread(int(matches[0]))
+
+        if completion_thread:
+            # スレッドが存在すればスレッドを閉じる
+            await completion_thread.edit(archived=True)
+            # 削除ボタン追加
+            completion_button = discord.ui.Button(label="完了", style=discord.ButtonStyle.success, disabled=True)
+            delete_button = discord.ui.Button(label="削除", style=discord.ButtonStyle.danger, custom_id="support_delete")
+            view = discord.ui.View()
+            view.add_item(completion_button)
+            view.add_item(delete_button)
+            await interaction.edit_original_response(content=interaction.message.content, view=view)
+
+    async def delete_thread(self, interaction: discord.Interaction):
+        # 押されたボタンから対象のスレッドを取得
+        pattern = r"\`(.*?)\`"
+        matches = re.findall(pattern, interaction.message.content)
+        completion_thread = await interaction.guild.fetch_channel(matches[0])
+
+        if len(matches) == 1:
+            # 1回目のボタンだったら再確認する
+            original_text = interaction.message.content
+            await interaction.edit_original_response(content=original_text + "\n`削除するにはもう一度押してください`")
+
+            # 5秒の猶予
+            await asyncio.sleep(5)
+
+            # 削除取り消し
+            await interaction.edit_original_response(content=original_text)
+            return
+        
+        # 2回目以降は「`削除するにはもう一度押してください`」によりmatchesが2になるため削除する
+        
+        if completion_thread:
+            # スレッドが存在すればスレッドを削除する
+            await completion_thread.delete()
+            
+            # お知らせメッセージも削除
+            await interaction.delete_original_response()
 
 async def setup(bot):
     await bot.add_cog(ButtonResponse(bot))
